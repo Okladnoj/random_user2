22fc7b088ad66186a0e3c58c6970c240efb1e204
Дмитрий <36802187+Okladnoj@users.noreply.github.com>
2019-11-14 00:02:06 +0200

    1

--- lib/pages/detal_image.dart
+++ lib/pages/detal_image.dart
@@ -18,66 +18,58 @@ class ImageDetalisPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('${userModel.titlename} ${userModel.lastname}'),
       ),
-      body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.all(10.0),
-              constraints: BoxConstraints.tightFor(height: 350, width: 350),
-              child: Image.network(
-                this.assetPath,
-                fit: BoxFit.fill,
-              ),
-            ),
-            Container(
-                margin: const EdgeInsets.all(3.0),
-                child: Text.rich(
-                  TextSpan(children: <TextSpan>[
-                    TextSpan(
-                        text: '${userModel.firstname} ',
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-                    TextSpan(
-                      text: 'age: ',
-                    ),
-                    TextSpan(
-                        text: '${userModel.age}',
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-                  ]),
-                  textScaleFactor: 2.5,
-                )),
-            Container(
-                margin: const EdgeInsets.all(1.0),
-                child: Text(
-                    '${userModel.titlename} ${userModel.firstname} ${userModel.lastname}',
-                    textScaleFactor: 2)),
-            Container(
-                margin: const EdgeInsets.all(1.0),
-                child: Text.rich(
-                    TextSpan(children: <TextSpan>[
-                      TextSpan(
-                          text:
-                              '${userModel.titlename} ${userModel.firstname} ${userModel.lastname} ',
-                          style: TextStyle(fontWeight: FontWeight.bold)),
-                      TextSpan(
-                        text: 'lives in the country of ',
-                      ),
-                      TextSpan(
-                          text: '${userModel.country}',
-                          style: TextStyle(fontWeight: FontWeight.bold)),
-                      TextSpan(
-                        text: ', the city of ',
-                      ),
-                      TextSpan(
-                          text: '${userModel.city},\n $_dict',
-                          style: TextStyle(fontWeight: FontWeight.bold)),
-                      TextSpan(
-                        text: ' km away from you.',
-                      ),
-                    ]),
-                    textScaleFactor: 1.4,
-                    textAlign: TextAlign.center))
-          ]),
+      body: ListView(padding: const EdgeInsets.all(8), children: <Widget>[
+        Container(
+          margin: const EdgeInsets.all(10.0),
+          constraints: BoxConstraints.tightFor(height: 350, width: 350),
+          child: Image.network(
+            this.assetPath,
+            fit: BoxFit.fill,
+          ),
+        ),
+        Container(
+            margin: const EdgeInsets.all(3.0),
+            child: Text.rich(
+              TextSpan(children: <TextSpan>[
+                TextSpan(
+                    text: '${userModel.firstname} ',
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                TextSpan(
+                  text: 'age: ',
+                ),
+                TextSpan(
+                    text: '${userModel.age}',
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ]),
+              textScaleFactor: 2.5,
+            )),
+        Container(
+            margin: const EdgeInsets.all(1.0),
+            child: Text.rich(
+                TextSpan(children: <TextSpan>[
+                  TextSpan(
+                      text:
+                          '${userModel.titlename} ${userModel.firstname} ${userModel.lastname} ',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  TextSpan(
+                    text: 'lives in the country of ',
+                  ),
+                  TextSpan(
+                      text: '${userModel.country}',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  TextSpan(
+                    text: ', the city of ',
+                  ),
+                  TextSpan(
+                      text: '${userModel.city},\n $_dict',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  TextSpan(
+                    text: ' km away from you.',
+                  ),
+                ]),
+                textScaleFactor: 1.4,
+                textAlign: TextAlign.center))
+      ]),
     );
   }
 }
